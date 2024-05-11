69e26a8bfe33b0d0309717f0129729e981b64a19
Gözde <gkahraman03@gmail.com>
2024-05-11 17:24:46 +0300

    IDs added

--- src/main/java/com/example/iae/MainPageController.java
+++ src/main/java/com/example/iae/MainPageController.java
@@ -1,11 +1,40 @@
 package com.example.iae;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class MainPageController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Button addFolderButton;
+    @FXML
+    private Button newProjectButton;
+    @FXML
+    private Button configSetButton;
+    @FXML
+    private Button runButton;
+
+    @FXML
+    private RadioButton onlyRun;
+    @FXML
+    private RadioButton compNrun;
+
+    @FXML
+    private TextArea correctResultArea;
+
+    @FXML
+    private TextField compCmdText;
+    @FXML
+    private TextField runCmdText;
+
+    @FXML
+    private TableColumn<?, ?> outcomeCol;
+    @FXML
+    private TableColumn<?, ?> scoreCol;
+    @FXML
+    private TableColumn<?, ?> stdIDcol;
 
     @FXML
     protected void onHelloButtonClick() {
