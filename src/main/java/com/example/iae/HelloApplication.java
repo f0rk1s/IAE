14fe1b2c17c5a3f39fb854367c36a26c53e1fb54
RennieFlc <105913262+RennieFlc@users.noreply.github.com>
2024-05-11 16:41:15 +0300

    Added Config Page

--- src/main/java/com/example/iae/HelloApplication.java
+++ src/main/java/com/example/iae/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 618);
+        stage.setTitle("IAE");
         stage.setScene(scene);
         stage.show();
     }
