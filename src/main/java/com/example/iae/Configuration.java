b133e67c50d8405724a0ccfcded549f99095a26d
RennieFlc <105913262+RennieFlc@users.noreply.github.com>
2024-05-11 20:06:09 +0300

    Added some GUI functionalities

--- src/main/java/com/example/iae/Configuration.java
+++ src/main/java/com/example/iae/Configuration.java
@@ -41,6 +41,9 @@ public class Configuration {
         }
     }
 
+    public Configuration(String language, String command) {
+    } //THIS IS LIKELY TO BE DELETED IN FUTURE??
+
     public String getName() {
         return name;
     }
